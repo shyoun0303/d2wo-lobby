5e5cd25f4a0453eddb86876032735af8e54a4709
shyoun0303 <shyoun0303@gmail.com>
2022-04-01 01:00:48 +0900

    zone append

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,98 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA)
+    console.log('Player tags: ', WA.player.tags)
+    
     
     WA.room.onEnterLayer('clockZone').subscribe(() => {
         const today = new Date();
+
+        const year = today.getFullYear(); // 년도
+        const month = today.getMonth() + 1;  // 월
+        const date = today.getDate();  // 날짜
+        const day = today.getDay();  // 요일
+
+
+        const daydate = year + '.' + month + '.' + date;
         const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+        currentPopup = WA.ui.openPopup("clockPopup", daydate + " " + time,[]);
     })
 
     WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+
+
+
+        
+    WA.room.onEnterLayer('game01Zone').subscribe(() => {
+        console.log('game01Zone onEnterLayer');
+        const triggerMessage = WA.ui.displayActionMessage({
+            message: "Press SPACE to play Games",
+            callback: () => {
+                WA.nav.openCoWebSite("https://gamesnacks.com/embed", true, "allowfullscreen");
+            }
+        });
+        
+        setTimeout(() => {
+            // later
+            triggerMessage.remove();
+        }, 3000);
+        
+    })
+    WA.room.onLeaveLayer('game01Zone').subscribe(() => {
+        console.log('game01Zone onLeaveLayer');
+        
+    })
+    
+
+
+    WA.room.onEnterLayer('game02Zone').subscribe(() => {
+        console.log('game02Zone onEnterLayer');
+
+        const triggerMessage = WA.ui.displayActionMessage({
+            message: "Press SPACE to play Games",
+            callback: () => {
+                WA.nav.openCoWebSite("https://gamesnacks.com/embed", true, "allowfullscreen");
+            }
+        });
+        
+        setTimeout(() => {
+            // later
+            triggerMessage.remove();
+        }, 3000);
+       
+        
+    })
+    WA.room.onLeaveLayer('game02Zone').subscribe(() => {
+        console.log('game02Zone onLeaveLayer');
+    })
+    
+
+
+    WA.room.onEnterLayer('game03Zone').subscribe(() => {
+        console.log('game03Zone onEnterLayer');
+
+        const triggerMessage = WA.ui.displayActionMessage({
+            message: "Press SPACE to play Games",
+            callback: () => {
+                WA.nav.openCoWebSite("https://buddyboardgames.com", true, "allowfullscreen");
+            }
+        });
+        
+        setTimeout(() => {
+            // later
+            triggerMessage.remove();
+        }, 3000);
+       
+        
+    })
+    WA.room.onLeaveLayer('game03Zone').subscribe(() => {
+        console.log('game03Zone onLeaveLayer');
+    })
+
+    
+
+
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
